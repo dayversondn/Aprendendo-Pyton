954c9bdfeb1a7a0643e71d2fcb22a3ccd98ff14a
Dayverson Silva <deyversondn15@gmail.com>
2023-02-17 06:09:15 -0300

    criando sistema de pontuação

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,8 @@ print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
 
 print("Qual o nível de dificuldade?")
 print("(1) Fácil (2) Médio (3) Difícil")
@@ -35,14 +36,15 @@ for rodada in range (1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
     
     rodada = rodada  + 1 
-
 print("Fim do jogo!")
