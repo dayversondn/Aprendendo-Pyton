37607365127d6fe4e8f86402a4593a634e2d5788
Dayverson Silva <deyversondn15@gmail.com>
2023-02-18 06:57:51 -0300

    Chamando as funções dos jogos adivinhação e forca

--- jogos.py
+++ jogos.py
@@ -1,16 +1,21 @@
 import forca
 import adivinhacao
 
-print("********************************")
-print("*******Escolha o seu jogo*******")
-print("********************************")
+def escolher_jogo():
+    print("********************************")
+    print("*******Escolha o seu jogo*******")
+    print("********************************")
 
-print("(1) Forca (2) Adivinahção")
+    print("(1) Forca (2) Adivinahção")
 
-jogo = int(input("Qual o jogo deseja escolher?"))
+    jogo = int(input("Qual o jogo deseja escolher?"))
 
-if (jogo == 1):
-    print("Jogando forca!")
-elif(jogo == 2):
-    print("Jogando adivinhação!")
-
+    if (jogo == 1):
+        print("Jogando forca!")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação!")
+        adivinhacao.jogar()
+        
+if(__name__ == "__main__"):
+    escolher_jogo()
